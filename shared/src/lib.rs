8eea16d213b00d1ffa7452fa70439ba97501293b
Massimo Fraschetti <subscriptions@fraschetti.me>
2025-02-10 20:15:36 -0500

    clean up lib.rs

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -8,15 +8,8 @@ pub mod events;
 pub mod middleware;
 pub mod utils;
 
-// mod middleware {
-//     mod livekit_handler;
-//     pub use livekit_handler::LiveKitHandler;
-// }
-
-use app::{Effect, View};
 use lazy_static::lazy_static;
 use middleware::LiveKitMiddleware;
-// use middleware::LiveKitHandler;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 pub use crux_core::{bridge::Bridge, Core, Request};
@@ -24,61 +17,7 @@ pub use crux_http as http;
 
 pub use app::{App, Event};
 
-// Define CoreLike trait
-// pub trait CoreLike {
-//     fn handle_event(&self, event: Event) -> Effect;
-//     fn handle_response(&self, id: u32, data: &[u8]) -> Effect;
-//     fn view(&self) -> View;
-// }
-
 uniffi::include_scaffolding!("shared");
-
-// lazy_static! {
-//     static ref HANDLER: LiveKitHandler = LiveKitHandler {
-//         core: Core::new(),
-//         runtime: tokio::runtime::Builder::new_multi_thread()
-//             .enable_all()
-//             .build()
-//             .unwrap(),
-//     };
-//     static ref CORE: Bridge<LiveKitHandler> = Bridge::new(HANDLER.clone());
-// }
-
-// lazy_static! {
-//     static ref HANDLER: LiveKitHandler = LiveKitHandler::new(Core::new());
-//     static ref CORE: Bridge<LiveKitHandler> = Bridge::new(HANDLER.clone());
-// }
-
-// lazy_static! {
-//     static ref CORE: Bridge<Core<App>> = Bridge::new(Core::new());
-//     static ref RUNTIME: tokio::runtime::Runtime = tokio::runtime::Builder::new_multi_thread()
-//         .enable_all()
-//         .build()
-//         .unwrap();
-// }
-
-// lazy_static! {
-//     static ref HANDLER: LiveKitHandler = {
-//         let core = Core::new();
-//         let context = CapabilityContext::new();
-//         LiveKitHandler::new(core, context)
-//     };
-//     static ref CORE: Bridge<LiveKitHandler> = Bridge::new(HANDLER.clone());
-// }
-
-// lazy_static! {
-//     // Core wrapped by LiveKitMiddleware, then given to Bridge
-//     static ref CORE: Bridge<LiveKitMiddleware<Core<App>>> = {
-//         // Create the base Core
-//         let core = Core::new();
-//
-//         // Wrap it in our LiveKit middleware
-//         let middleware = LiveKitMiddleware::new(core);
-//
-//         // Create the Bridge with the middleware
-//         Bridge::new(middleware)
-//     };
-// }
 
 lazy_static! {
     static ref CORE: Bridge<LiveKitMiddleware<Core<App>>> = {
@@ -87,15 +26,6 @@ lazy_static! {
         Bridge::new(middleware)
     };
 }
-
-// lazy_static! {
-//     static ref HANDLER: LiveKitHandler = {
-//         let core = Core::new();
-//         let context = CapabilityContext::new();
-//         LiveKitHandler::new(core, context)
-//     };
-//     static ref CORE: Bridge<LiveKitHandler> = Bridge::new(HANDLER.clone());
-// }
 
 #[wasm_bindgen]
 pub fn process_event(data: &[u8]) -> Vec<u8> {
